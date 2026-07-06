5dac4b9654486fdea3bee9285177ac0168f9f47c
Aryansh Jain <aryanshjain1412@gmail.com>
2026-07-06 23:30:03 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Nav from '../../hooks/src/components/Nav'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Nav/>
+    </>
+  )
+}
+
+export default App
